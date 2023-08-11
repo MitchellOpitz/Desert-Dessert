76bdfbcd45287914604be65818e01d9f7f7085df
TuckleberryWinn <63176475+TuckleberryWinn@users.noreply.github.com>
2023-08-11 17:48:29 -0600

    Work on driving section
    Worked on driving scene
    -Cleaned up road sprites so they connect a bit better
    -Redid road colliders to avoid getting caught
    -Adjusted procedural road tiles to prevent unfinishable levels
    -Added a little lead to the camera when you get to higher speeds

--- Assets/Scripts/Driving/CameraBasicFollow.cs
+++ Assets/Scripts/Driving/CameraBasicFollow.cs
@@ -4,9 +4,32 @@ using UnityEngine;
 
 public class CameraBasicFollow : MonoBehaviour
 {
-    [SerializeField] Transform Target;
-    void LateUpdate() {
-        transform.position = Target.position + new Vector3(0, 0, -10);
-        transform.rotation = Target.rotation;
+    [SerializeField]
+    private Transform Car;
+
+    [SerializeField]
+    private Transform LeadingTarget;
+
+    [SerializeField]
+    private Rigidbody2D carRB;
+
+    private float speedLeadFactor;
+    private Vector3 velocity = Vector3.zero;
+    private Vector3 adjustedTarget;
+
+    void LateUpdate()
+    {
+        speedLeadFactor = carRB.velocity.magnitude / 30;
+        if (speedLeadFactor < 0.3)
+        {
+            speedLeadFactor = 0;
+        }
+        speedLeadFactor -= 0.3f;
+        Debug.Log(speedLeadFactor);
+
+        adjustedTarget = Vector3.Lerp(Car.position, LeadingTarget.position, speedLeadFactor);
+
+        transform.position = adjustedTarget + new Vector3(0, 0, -10);
+        transform.rotation = Car.rotation;
     }
 }
